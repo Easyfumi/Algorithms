460c988a9fbe9f9d3c33b5c62379a090519ca1c3
Easyfumi <oleg.marinin.49@yandex.ru>
2024-05-08 22:36:47 +0300

    quick sort Lomuto

---
+++ Algorithms/src/main/java/ru/marinin/qSortL.java
@@ -0,0 +1,50 @@
+package ru.marinin;
+
+import java.util.Scanner;
+
+public class qSortL {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        quickSort(array, 0, array.length - 1);
+
+        for (int el : array) {
+            System.out.println(el);
+        }
+    }
+
+     public static void quickSort(int[] nums, int left, int right) {
+         if (right - left < 1) {
+             return;
+         }
+         int p = partition(nums, left, right);
+         quickSort(nums,left,p-1);
+         quickSort(nums,p+1,right);
+     }
+
+
+
+     public static int partition(int[] nums, int left, int right) {
+         int pivot = nums[right];
+         int i = left;
+         for (int j = left; j <= right; j++) {
+             if (nums[j] > pivot) {
+                 int temp = nums[i];
+                 nums[i] = nums[j];
+                 nums[j] = temp;
+                 i++;
+             }
+         }
+         nums[right] = nums[i];
+         nums[i] = pivot;
+
+         return i;
+     }
+}
