073e7df13e26477716eb381aad6b8c62bb1ea4b0
Easyfumi <oleg.marinin.49@yandex.ru>
2024-05-24 21:03:12 +0300

    Backpack task

--- Algorithms/src/main/java/ru/marinin/Backpack.java
+++ Algorithms/src/main/java/ru/marinin/Backpack.java
@@ -1,6 +1,5 @@
 package ru.marinin;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Backpack {   //рюкзак и поиск максимального веса
@@ -8,6 +7,14 @@ public class Backpack {   //рюкзак и поиск максимальног�
         Scanner scanner = new Scanner(System.in);
         String firstLine = scanner.nextLine();
         String secondLine = scanner.nextLine();
+
+        int[][] array = initArray(firstLine, secondLine);
+        int rez = calculation(array);
+        System.out.println(rez);
+
+    }
+
+    public static int[][] initArray(String firstLine, String secondLine) {
         String[] firstLineStr = firstLine.split(" ");
         String[] secondLineStr = secondLine.split(" ");
         int w = Integer.parseInt(firstLineStr[0]);
@@ -16,12 +23,6 @@ public class Backpack {   //рюкзак и поиск максимальног�
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(secondLineStr[i]);
         }
-
-        System.out.println(calculation(nums, w, n));
-
-    }
-
-    public static int calculation(int[] nums, int w, int n) {
         int[][] array = new int[n + 1][w + 1];
         array[0][0] = 0;
         for (int i = 1; i < w + 1; i++) {
@@ -30,15 +31,17 @@ public class Backpack {   //рюкзак и поиск максимальног�
         for (int i = 1; i < n + 1; i++) {
             array[i][0] = nums[i - 1];
         }
-
-
         for (int i = 1; i < w + 1; i++) {
             if (array[0][i] >= array[1][0]) {
                 array[1][i] = array[1][0];
             }
         }
+        return array;
+    }
 
-
+    public static int calculation(int[][] array) {
+        int n = array.length - 1;
+        int w = array[0].length - 1;
         for (int i = 2; i < n + 1; i++) {
             for (int j = 1; j < w + 1; j++) {
                 int tW = array[0][j];
@@ -46,21 +49,17 @@ public class Backpack {   //рюкзак и поиск максимальног�
                 if (tW < tN) {
                     array[i][j] = array[i - 1][j];
                 } else {
-                    if (array[i-1][j]+tN<=tW) {
-                        array[i][j]=array[i-1][j]+tN;
+                    if (Math.max(array[i - 1][j], array[i - 1][j - tN] + tN) <= tW) {
+                        array[i][j] = Math.max(array[i - 1][j], array[i - 1][j - tN] + tN);
                     } else {
-                        array[i][j]=Math.max(array[i-1][j], tN);
+                        array[i][j] = Math.max(array[i - 1][j], tN);
                     }
                 }
-
             }
         }
-
-
-
-        for(
-    int[] line :array)
-            System.out.println(Arrays.toString(line));
-        return 0;
-}
+//        for (int[] line : array) {
+//            System.out.println(Arrays.toString(line));
+//        }
+        return array[n][w];
+    }
 }
