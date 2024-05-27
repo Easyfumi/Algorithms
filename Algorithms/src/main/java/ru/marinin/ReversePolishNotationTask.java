898782d82ac1faaaed6b539d82afc855e81705c8
Easyfumi <oleg.marinin.49@yandex.ru>
2024-05-27 21:24:05 +0300

    ReversePolishNotation

---
+++ Algorithms/src/main/java/ru/marinin/ReversePolishNotationTask.java
@@ -0,0 +1,31 @@
+package ru.marinin;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReversePolishNotationTask {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            if (!line.equals("*") && !line.equals("+") && !line.equals("-") && !line.equals(":")) {
+                stack.push(Integer.parseInt(line));
+            } else if (line.equals("+")) {
+                stack.push(stack.pop()+stack.pop());
+            } else if (line.equals("-")) {
+                stack.push(-stack.pop()+stack.pop());
+            } else if (line.equals("*")) {
+                stack.push(stack.pop()*stack.pop());
+            } else if (line.equals(":")) {
+                int first = stack.pop();
+                int second = stack.pop();
+                stack.push(second/first);
+            }
+        }
+        System.out.println(stack.pop());
+    }
+}
+
+
